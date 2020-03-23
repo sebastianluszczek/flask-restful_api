d369be4c072a5f18fc413030338f6d2599ae7bba
Sebastian Łuszczek <sebastianluszczek@pop-os.localdomain>
2020-03-23 15:43:41 +0100

    flask-restful-with-sqlalchemy

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api, reqparse
-
-from uuid import uuid4
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sebastian:pass123@localhost/flask_restful_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
 api = Api(app)
 
-items = []
+class ItemModel(db.Model):
+    __tablename__ = 'items'
+    
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
+
+class ItemSchema(ma.Schema):
+    class Meta:
+        fields = ('_id', 'name', 'price', '_links')
+
+    # Smart hyperlinking
+    _links = ma.Hyperlinks(
+        {"self": ma.URLFor('item', _id="<_id>"), "collection": ma.URLFor('items')}
+    )
+
+items_schema = ItemSchema(many=True)
+item_schema = ItemSchema()
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', type=str, required=True)
@@ -15,37 +42,45 @@ parser.add_argument('price', type=float, required=True)
 
 class Items(Resource):
     def get(self):
-        return {'items': items}, 200
+        items = ItemModel.query.all()
+
+        return {'items': items_schema.dump(items)}, 200
 
     def post(self):
         data = parser.parse_args()
-        data['_id'] = str(uuid4())
-        items.append(data)
-        return {'item': data}, 201
+        item = ItemModel(**data)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return {'item': item_schema.dump(item)}, 201
 
 class Item(Resource):
     def get(self, _id):
-        item = next(filter(lambda x: x['_id'] == _id, items), None)
-        return {'item': item}, 200 if item else 404
-    
-    def put(self, _id):
-        data = parser.parse_args()
-        item = next(filter(lambda x: x['_id'] == _id, items), None)
+        item = ItemModel.query.filter_by(_id = _id).first_or_404(f'No item with _id: {_id}')
 
-        if item:
-            item.update(data)
-            return {'item': item}, 200
-        else:
-            data['_id'] = _id
-            items.append(data)
-            return {'item': data}, 201
+        return {'item': item_schema.dump(item)}, 200
+
+    def put(self, _id):
+        item = ItemModel.query.filter_by(_id = _id).first_or_404(f'No item with _id: {_id}')
+
+        data = parser.parse_args()
+        for key, val in data.items():
+            setattr(item, key, val)
+
+        db.session.commit()
+        
+        return {'item': item_schema.dump(item)}, 201
 
     def delete(self, _id):
-        global items
-        items = list(filter(lambda x: x['_id'] != _id, items))
-        return {'message': f'item {_id} deleted'}, 200
+        item = ItemModel.query.filter_by(_id = _id).first()
+
+        db.session.delete(item)
+        db.session.commit()
+
+        return {'message': 'item deleted'}, 200
         
 
-api.add_resource(Items, '/items')
-api.add_resource(Item, '/items/<string:_id>')
+api.add_resource(Items, '/items', endpoint='items')
+api.add_resource(Item, '/items/<_id>', endpoint='item')
 
